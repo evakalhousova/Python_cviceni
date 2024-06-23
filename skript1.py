139bac4eb737f69f2fb10b86b8f47d4745321910
evakalhousova <eva.kalhousova@gmail.com>
2024-06-23 10:05:22 +0200

    Test - podmínky

--- skript1.py
+++ skript1.py
@@ -1,1 +1,12 @@
-print('test')
+cislo = 15
+
+zbytek_3 = cislo % 3
+zbytek_5 = cislo % 5
+if zbytek_3 == 0 and zbytek_5 == 0:
+    print("FizzBuzz")
+elif zbytek_3 == 0:
+    print("Fizz")
+elif zbytek_5 == 0:
+    print("Buzz")
+else:
+    print(cislo)
